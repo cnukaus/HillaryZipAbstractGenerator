f124d92b025db0a1bce67e4b8a05099967d6377b
XY <cnukaus@gmail.com>
2016-09-20 07:47:30 +1000

    First commit

---
+++ Zip_extract_then_sort_filename.py
@@ -0,0 +1,59 @@
+#http://stackoverflow.com/questions/2212643/python-recursive-folder-read
+
+#if zip/RAR files are encrypted, user need to press Enter manualy to skip
+
+import os
+import sys
+import subprocess
+import collections
+import pandas as pd
+import matplotlib
+import seaborn as sns
+
+rootdir = sys.argv[1]
+ExtractDir = sys.argv[2]
+
+all_filename=[]
+all_fileext=[]
+all_filewords=[]
+
+print "When program pauses (encountering zip/RAR files that are encrypted), user need to press Enter manualy to skip/abort"
+
+for folder, subs, files in os.walk(rootdir):
+    
+        for filename in files:
+            
+                  list1=filename.split('.')
+                  l2=["rar","zip"]#set()
+                  if list1[-1] in l2:#
+                     subprocess.check_call(["7z.exe","x",os.path.join(folder,filename) ,"-r","-O"+ExtractDir])
+                  else:
+                     src=open(os.path.join(folder, filename), 'r')
+                     dest=open(os.path.join(ExtractDir, filename), 'w')  #What if file duplicate??
+                     dest.write(src.read())
+                     dest.close()
+                     src.close()
+
+
+                
+
+for folder, subs, files in os.walk(ExtractDir):
+   for filename in files:
+
+      list_words=''.join(list1[0:filename.count('.')]).replace(' ','_').split('_')  #remove suffix. Underscore, not space is dividing file names
+      all_filewords.append(list_words) #file title,how to append one element by 1
+      all_filename.append(list1[0:filename.count('.')])
+      all_fileext.append(list1[-1])
+                
+                 
+counter=collections.Counter(all_fileext)
+
+
+df=pd.DataFrame([counter.values(),counter.keys()])
+df2=df.transpose()
+df2.columns=['cnt','fname']
+df3=df2.sort_values(['cnt'],ascending=[True])
+print df3
+df2.head()
+ax = sns.barplot(x="fname", y="cnt", data=df3)
+sns.plt.show()
